4e2bc616119ec527b6524cb5d2802a2ba5391451
Khomkrid.ler <khomkrid.ler@ascendcorp.com>
2023-11-09 20:23:08 +0700

    Initial Commit

---
+++ dev.aofiee.TestFCM/dev_aofiee_TestFCMApp.swift
@@ -0,0 +1,17 @@
+//
+//  dev_aofiee_TestFCMApp.swift
+//  dev.aofiee.TestFCM
+//
+//  Created by DD.Hakka on 9/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct dev_aofiee_TestFCMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
